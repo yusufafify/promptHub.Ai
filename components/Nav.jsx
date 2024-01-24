afaf388338ba985b5d6e5df7ec0b143032f7bcac
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-01-24 18:26:33 +0200

    image tag changed

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image
+        <img
           src="/assets/images/logo.png"
           alt="logo"
           width={50}
