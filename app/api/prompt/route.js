069d7bca6bb117030e8c103c0cf13dd5d6444e70
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-01-24 18:04:33 +0200

    test

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,6 +6,7 @@ export const GET = async (request) => {
         await connectToDB()
 
         const prompts = await Prompt.find({}).populate('creator')
+        console.log(prompts)
 
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
